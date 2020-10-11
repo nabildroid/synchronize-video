a56947530b030516601d917adbefcd79b60f33b6
nabildroid <pni20156789@gmail.com>
2020-10-11 12:53:21 +0100

    add basic routing

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -2,10 +2,27 @@ import React from "react"
 import AppProvider from "./contexts/appContext"
 import ServerProvider from "./contexts/serverContext"
 import JoinView from "./views/join_view"
+import { HashRouter as Router, Route, Redirect } from "react-router-dom";
+import P2PProvider from "./contexts/p2pContext";
+import RoomView from "./views/room_view";
+
+
+
 const App = () => {
     return (
         <div>
-            <JoinView />
+            <Router>
+                <Route path="/join">
+                    <JoinView />
+                </Route>
+                <Route path="/room">
+                    <P2PProvider>
+                        <RoomView />
+                    </P2PProvider>
+                </Route>
+
+                <Redirect from="*" to="/join" />
+            </Router>
 
         </div>
     )
