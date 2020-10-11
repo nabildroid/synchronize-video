c2c5d324959ab8b484fa234043b979cd522bbbbf
nabildroid <pni20156789@gmail.com>
2020-10-11 20:44:38 +0100

    change appContext and add appAction

---
+++ public/src/actions/appAction.ts
@@ -0,0 +1,5 @@
+import { IAppState, AppActions } from "../models/app_model";
+
+export default (state: IAppState, action: AppActions) => {
+	return state;
+};
--- public/src/contexts/appContext.tsx
+++ public/src/contexts/appContext.tsx
@@ -1,14 +1,21 @@
-import React, { createContext } from "react"
-import { IAppProvider } from "../models/app_model"
+import React, { createContext, useReducer } from "react"
+import AppAction from "../actions/appAction"
+import { AppStateInit, IAppProvider } from "../models/app_model"
 
 
 
-export const AppContext = createContext<IAppProvider>({})
+export const AppContext = createContext<IAppProvider>(null)
+
 
 const AppProvider: React.FC = ({ children }) => {
 
+    const [state, dispatch] = useReducer(AppAction, AppStateInit);
+
+    const values = {
+        ...state
+    }
     return (
-        <AppContext.Provider value={{}}>
+        <AppContext.Provider value={values}>
             {children}
         </AppContext.Provider>
     )
