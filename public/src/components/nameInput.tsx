d2a0697e6bc75dff39c2d80b0cfd4b9655b2a320
nabildroid <pni20156789@gmail.com>
2020-11-05 19:05:29 +0100

    missing a dependency

--- public/src/components/nameInput.tsx
+++ public/src/components/nameInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RandomName from "../utils/randomName";
 import Label from "./label";
 import Svg from "./svg";
