949f5767114d461edb8632022600c04f3b083402
nabildroid <pni20156789@gmail.com>
2020-11-10 20:08:09 +0100

    get the last visible MessageTimeLabel in messages list

--- public/src/components/messageTimeLabel.tsx
+++ public/src/components/messageTimeLabel.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { TimelineMessage } from "../types/message_type";
 import { Duration } from "../types/video_type";
+import TwoDigits from "../utils/twoDigitsNumber";
 
 
 type Props = {
-    time: Duration
+    time: Duration,
+    siblings: React.MutableRefObject<{
+        elm: HTMLDivElement,
+        time: Duration
+    }[]>
 }
 
-const MessageTimeLabel: React.FC<Props> = ({ time }) => {
+const MessageTimeLabel: React.FC<Props> = ({ time, siblings }) => {
+    const elm = useRef<HTMLDivElement>()
+
+    useEffect(() => {
+        if (elm.current && siblings.current) {
+            siblings.current.push({ elm: elm.current, time });
+            return () => siblings.current = siblings.current.filter(e => e.elm != elm.current)
+        }
+    }, [elm, siblings]);
+    
     return (
-        <div className="font-meduim flex items-center w-12 px-1 mx-auto space-x-1 text-xs text-indigo-400 bg-indigo-200 rounded-full">
+        <div ref={elm} className="font-meduim flex items-center w-12 px-1 mx-auto space-x-1 text-xs text-indigo-400 bg-indigo-200 rounded-full">
             <span className="w-2 h-2 bg-indigo-300 rounded-full"></span>
-            <time >{time.minute}:{time.secoud}</time>
+            <time >{TwoDigits(time.minute)}:{TwoDigits(time.secoud)}</time>
         </div>
     )
 }
--- public/src/layouts/messages/messagesList.tsx
+++ public/src/layouts/messages/messagesList.tsx
@@ -1,9 +1,10 @@
-import React from "react"
-import { MessageType, TimelineMessages } from "../../types/message_type";
+import React, { useRef } from "react"
+import { MessageType, TimelineMessage, TimelineMessages } from "../../types/message_type";
 import Dot from "../../components/messageDot";
 import TextMessage from "./textMessage";
 import EmojiMessage from "./emojiMessage";
 import MessageTimeLabel from "../../components/messageTimeLabel";
+import { Duration } from "../../types/video_type";
 
 
 
@@ -13,15 +14,46 @@ type Props = {
 
 
 const MessagesList: React.FC<Props> = ({ content }) => {
+
+    const childs = useRef<{
+        elm: HTMLDivElement,
+        time: Duration
+    }[]>([]);
+
+    const handleScroll = (event: React.UIEvent) => {
+        const parent = event.target as HTMLDivElement;
+        const viewportStart = parent.scrollTop;
+        const viewportEnd = viewportStart + parent.offsetHeight;
+
+        childs.current.forEach(e => {
+            e.elm.classList.remove("bg-red-200")
+            e.elm.classList.add("bg-indigo-200")
+        });
+        for (const { elm, time } of Array.from(childs.current).reverse()) {
+            const { offsetHeight: height, offsetTop: top } = elm;
+            if (top >= viewportStart && top + height / 2 <= viewportEnd) {
+                // console.log(elm);
+                elm.classList.remove("bg-indigo-200")
+
+                elm.classList.add("bg-red-200");
+                break;
+            }
+        }
+
+    }
+
     return (
-        <div className="md:pb-2 md:space-y-2 flex-1 max-h-screen pb-24 space-y-1 overflow-y-scroll text-center">
+        <div
+            className="md:pb-2 md:space-y-2 flex-1 max-h-screen pb-24 space-y-1 overflow-y-scroll text-center"
+            onScroll={handleScroll}
+        >
             {
                 content.map(elm => {
                     switch (elm.type) {
                         case "dot":
                             return <Dot key={elm.id} />
                         case "time":
-                            return <MessageTimeLabel time={elm.payload} key={elm.id} />
+                            return <MessageTimeLabel siblings={childs} time={elm.payload} key={elm.id} />
                         case "message":
                             return elm.payload.body.type == MessageType.TEXT ?
                                 <TextMessage content={elm.payload.body.content} user={elm.payload.user.name} key={elm.id} /> :
